506d8bf2efe1b02e0aca35ac45e95157ce860dcb
Carlos Matheu Armengol <carlos@stage.tech>
2018-11-08 10:05:38 +0100

    connect vue socket io to store

--- vue-socket-client/src/main.js
+++ vue-socket-client/src/main.js
@@ -1,7 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex';
+import VueSocketio from 'vue-socket.io';
+import counterModule from './modules/counter';
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    io: {}
+  },
+  mutations: {
+    setSocket: (state, socket) => {
+      console.log('socket is connected');
+      state.io = socket;
+    }
+  },
+  modules: {
+    counterModule
+  }
+})
+
+Vue.use(VueSocketio, 'http://localhost:5000', store);
 
 new Vue({
   el: '#app',
+  store,
+  beforeCreate () {
+    store.commit('setSocket', this.$socket);
+  },
   render: h => h(App)
 })
---
+++ vue-socket-client/src/modules/counter.js
@@ -0,0 +1,29 @@
+const counterModule = {
+  state: {
+    count: 0
+  },
+  mutations: {
+    SOCKET_COUNTER_INCREMENT(state, counter) {
+      state.count = counter;
+    },
+    SOCKET_COUNTER_DECREMENT(state, counter) {
+      state.count = counter;
+    }
+  },
+  actions: {
+    socket_increment: ({
+      state,
+      rootState
+    }) => {
+      rootState.io.emit('increment', state.count);
+    },
+    socket_decrement: ({
+      state,
+      rootState
+    }) => {
+      rootState.io.emit('increment', state.count);
+    }
+  }
+};
+
+export default counterModule;
